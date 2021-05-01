4d839d8536d46e5ee57c36232d23d66a6cd1fd4a
GilOlensckiNeto <olensckineto@mail.usf.edu>
2021-05-01 04:08:46 -0400

    order/sales api

--- src/online-shop-api.js
+++ src/online-shop-api.js
@@ -62,3 +62,70 @@ export async function createUser(username, password, is_adm=false) {
           console.log(data);
         });
     }
+
+  export async function getOrders() {
+  const res = await fetch("http://localhost:3001/order/get")
+  const data = await res.json();
+  return data;
+}
+
+export async function createOrder(uID, value, date_of_order=Date.now()) {
+  const res = await fetch('http://localhost:3001/order/add', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({uID, value, date_of_order}),
+    });
+    const data = res.text();
+    return data;
+  }
+
+  export async function deleteOrder(id) {
+    fetch(`http://localhost:3001/order-delete/${id}`, {
+        method: 'DELETE',
+      })
+      .then(response => {
+          return response.text();
+      })
+      .then(data => {
+          getOrders();
+          console.log(data);
+      });
+  }
+
+  export async function getSales() {
+    const res = await fetch("http://localhost:3001/sales/get")
+    const data = await res.json();
+    return data;
+  }
+
+  export async function createSale(orderID, productID, product_count, date_of_sale) {
+    const res = await fetch('http://localhost:3001/sale/add', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({orderID, productID, product_count, date_of_sale}),
+      });
+      const data = res.text();
+      return data;
+    }
+
+    export async function getOrdersPerUser(uID) {
+      const res = await fetch('http://localhost:3001/order/get-history', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({uID}),
+        });
+        const data = await res.json();
+        return data;
+      }
+
+      export async function getSalesPerProduct() {
+        const res = await fetch("http://localhost:3001/sales/get-product")
+        const data = await res.json();
+        return data;
+      }
